01138e4d205864e26e0cf9d6a481f0dc4ebafd06
DanielPJFaria <dfaria@xldb.di.fc.ul.pt>
2015-08-18 15:34:07 +0100

    New interactive selection algorithm

--- AgreementMakerLight/src/aml/filter/InteractiveSelector.java
+++ AgreementMakerLight/src/aml/filter/InteractiveSelector.java
@@ -20,12 +20,20 @@
 ******************************************************************************/
 package aml.filter;
 
-import java.util.Set;
+import java.util.Vector;
 
 import aml.AML;
 import aml.match.Alignment;
+import aml.match.BlockRematcher;
 import aml.match.Mapping;
+import aml.match.NeighborSimilarityMatcher;
+import aml.match.Rematcher;
+import aml.match.StringMatcher;
+import aml.match.WordMatcher;
 import aml.ontology.URIMap;
+import aml.settings.NeighborSimilarityStrategy;
+import aml.settings.SizeCategory;
+import aml.settings.WordMatchStrategy;
 import aml.util.Oracle;
 
 public class InteractiveSelector implements Selector
@@ -33,22 +41,33 @@ public class InteractiveSelector implements Selector
 	
 //Attributes
 	
-	//The alignments used as support to decide
-	//whether to ask for user feedback
-	private Set<Alignment> aligns;
+	//Selection thresholds
+	private final double HIGH_THRESH = 0.7;
+	private final double AVERAGE_THRESH = 0.2;
+	private double lowThresh = 0.45;
 	//Auxiliary variables
-	private double[] previousSignatureVector;
-	private int previousFeedback;
-	private boolean previousAgreement;
+	private SizeCategory size;
+	private Vector<Rematcher> auxMatchers;
+	private Vector<Alignment> auxAlignments;
 	
 //Constructors
 	
-	public InteractiveSelector(Set<Alignment> a)
+	public InteractiveSelector()
 	{
-		aligns = a;
-		previousSignatureVector = new double[aligns.size()];
-		previousFeedback = 0;
-		previousAgreement = false;
+		size = AML.getInstance().getSizeCategory();
+		//Construct the list of auxiliary (re)matchers, which will be used
+		//to compute auxiliary alignments used for interactive selection
+		auxMatchers = new Vector<Rematcher>();
+		auxMatchers.add(new WordMatcher(WordMatchStrategy.AVERAGE));
+		auxMatchers.add(new StringMatcher());
+		auxMatchers.add(new NeighborSimilarityMatcher(
+				NeighborSimilarityStrategy.DESCENDANTS,
+				!size.equals(SizeCategory.SMALL)));
+		auxMatchers.add(new NeighborSimilarityMatcher(
+				NeighborSimilarityStrategy.ANCESTORS,
+				!size.equals(SizeCategory.SMALL)));
+		if(size.equals(SizeCategory.HUGE))
+			auxMatchers.add(new BlockRematcher());
 	}
 	
 //Public Methods
@@ -58,90 +77,112 @@ public class InteractiveSelector implements Selector
 	{
 		long time = System.currentTimeMillis()/1000;
 		System.out.println("Performing Interactive Selection");
-		AML aml = AML.getInstance();
-		URIMap map = aml.getURIMap();
 		
+		//Setup:
+		//1) Sort the input alignment
+		a.sort();
+		//2) Initialize the final alignment
 		Alignment selected = new Alignment();
-		int falseCount = 0;
+		//3) Get the URIMap to convert ids to URIs
+		URIMap map = AML.getInstance().getURIMap();
+		//4) Start the query count and set the query limit
+		int queryCount = 0;
+		int queryLimit;
+		if(size.equals(SizeCategory.SMALL))
+			//For small ontologies, we allow queries covering up to half the mappings
+			//in the input alignment (though 5% is reserved for interactive repair)
+			queryLimit = (int)Math.round(a.size()*0.45);
+		else
+			//For larger ontologies, we allow queries covering only 20% of the mappings
+			//in the input alignment (though again 5% is reserved for interactive repair)
+			queryLimit = (int)Math.round(a.size()*0.15);
+		//5) Start the consecutive negative count and set the limit
+		int consecutiveNegativeCount = 0;
+		boolean updated = false;
+		int consecutiveNegativeLimit;
+		if(size.equals(SizeCategory.SMALL) && size.equals(SizeCategory.MEDIUM))
+			consecutiveNegativeLimit = 5;
+		else
+			consecutiveNegativeLimit = 10;
+		//6) Compute auxiliary alignments
+		auxAlignments = new Vector<Alignment>();
+		for(Rematcher r : auxMatchers)
+			auxAlignments.add(r.rematch(a));
+
+		//Select - for each mapping:
 		for(Mapping m : a)
 		{
-			double sim = m.getSimilarity();
-			if(sim >= 0.7)
-				selected.add(m);
-			else
+			//Get the ids and URIs
+			int sourceId = m.getSourceId();
+			int targetId = m.getTargetId();
+			String sourceURI = map.getURI(sourceId);
+			String targetURI = map.getURI(targetId);
+			double finalSim = m.getSimilarity();
+			//Compute the auxiliary parameters
+			double maxSim = 0.0;
+			double average = finalSim;
+			int support = 0;
+			for(int i = 0; i < auxAlignments.size(); i++)
 			{
-				String sourceURI = map.getURI(m.getSourceId());
-				String targetURI = map.getURI(m.getTargetId());
-
-				if(disagreement(m.getSourceId(),m.getTargetId()))
+				double sim = auxAlignments.get(i).getSimilarity(sourceId, targetId);
+				if(sim > maxSim)
+					maxSim = sim;
+				if(sim > 0)
+					support++;
+				if(i < 3)
+					average += sim;
+			}
+			average /= 4;
+			
+			boolean check = false;
+			if(finalSim >= HIGH_THRESH)
+			{
+				check = true;
+				if(support < 2 || selected.containsConflict(m))
 				{
-					if(Oracle.check(sourceURI,targetURI,m.getRelationship().toString()))
+					if(queryCount < queryLimit)
 					{
-						selected.add(m);
-						previousFeedback=1;
+						check = Oracle.check(sourceURI,targetURI,m.getRelationship().toString());
+						queryCount++;
 					}
 					else
+						check = false;
+				}
+			}
+			else if(finalSim >= lowThresh || maxSim >= lowThresh)
+			{
+				check = false;
+				if(queryCount < queryLimit)
+				{
+					if(support > 1 && average > AVERAGE_THRESH &&
+							!selected.containsConflict(m))
 					{
-						falseCount++;
-						previousFeedback=-1;
+						check = Oracle.check(sourceURI,targetURI,m.getRelationship().toString());
+						//Update the query count
+						queryCount++;
+						//Update the consecutive negative query count
+						if(check)
+							consecutiveNegativeCount = 0;
+						else
+							consecutiveNegativeCount++;
+						//If it exceeds the limit, raise the lowThresh to the current
+						//similarity value
+						if(!updated && consecutiveNegativeCount > consecutiveNegativeLimit)
+						{
+							if(finalSim > lowThresh)
+								lowThresh = finalSim;
+							updated = true;
+						}
 					}
 				}
-
+				else
+					break;
 			}
-			if(falseCount > a.size() * 0.35)
-				break;
+			if(check)
+				selected.add(sourceId, targetId, maxSim);
 		}
 		time = System.currentTimeMillis()/1000 - time;
 		System.out.println("Finished in " + time + " seconds");
 		return selected;
-	}
-	
-//Private Methods
-	
-	//Checks if there is disagreement between the support
-	//alignments for a given class pair
-	private boolean disagreement(int sourceId, int targetId)
-	{
-		double[] signatureVector = new double[aligns.size()];
-		int index = 0;
-		for(Alignment a : aligns)
-			signatureVector[index++] = a.getSimilarity(sourceId,targetId);
-		
-		double variance = variance(signatureVector);
-
-		if(variance < 0.04)
-		{
-			previousAgreement = false;
-			previousFeedback = 0;
-			previousSignatureVector = signatureVector;
-			return false;
-		}
-		else if(previousAgreement == true && previousFeedback == -1 &&
-					previousSignatureVector == signatureVector)
-		{
-			previousAgreement = true;
-			previousFeedback = -1;
-			previousSignatureVector = signatureVector;
-			return false;
-		}
-		else
-		{
-			previousAgreement = true;
-			previousSignatureVector = signatureVector;
-			return true;
-		}
-	}
-	
-	//Measures the variance between elements of an array
-	private double variance(double[] v)
-	{
-		double average = 0.0;
-		for(double d : v)
-			average += d;
-		average /= v.length;
-		double variance = 0.0;
-		for(double d : v)
-			variance += Math.pow(d - average,2);
-		return variance / (v.length - 1);
 	}
 }
