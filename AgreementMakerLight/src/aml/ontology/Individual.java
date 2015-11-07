5dadf8bc0e4288ccadc2cb77afc6db1e3460f8fb
DanielPJFaria <dfaria@xldb.di.fc.ul.pt>
2015-11-07 12:10:50 +0000

    Created class to represent Individuals

---
+++ AgreementMakerLight/src/aml/ontology/Individual.java
@@ -0,0 +1,97 @@
+/******************************************************************************
+* Copyright 2013-2015 LASIGE                                                  *
+*                                                                             *
+* Licensed under the Apache License, Version 2.0 (the "License"); you may     *
+* not use this file except in compliance with the License. You may obtain a   *
+* copy of the License at http://www.apache.org/licenses/LICENSE-2.0           *
+*                                                                             *
+* Unless required by applicable law or agreed to in writing, software         *
+* distributed under the License is distributed on an "AS IS" BASIS,           *
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    *
+* See the License for the specific language governing permissions and         *
+* limitations under the License.                                              *
+*                                                                             *
+*******************************************************************************
+* An Ontology individual (or instance).                                       *
+*                                                                             *
+* @author Catia Pesquita                                                      *
+* @date 06-11-2015                                                            *
+******************************************************************************/
+package aml.ontology;
+
+
+import java.util.Set;
+
+import aml.util.StringParser;
+import aml.util.Table2Set;
+
+public class Individual
+{
+	
+//Attributes
+	
+	private int index;
+	private String name;
+	//the table of properties indexes and their values for the instance
+	private Table2Set<Integer,String> dataValues;
+
+	
+//Constructors
+
+	/**
+	 * Constructs a new Individual with given id and name
+	 * @param i: the id of the Individual
+	 * @param n: the name of the Individual
+	 */
+	public Individual(int i, String n)
+	{
+		index = i;
+		//TODO: Check that it is really formula here
+		name = StringParser.normalizeFormula(n);
+		dataValues = new Table2Set<Integer,String>();
+	}
+	
+//Public Methods
+
+	/**
+	 * @return the index of this Individual
+	 */
+	public int getIndex()
+	{
+		return index;
+	}
+	
+	/**	
+	 * @return the name of this Individual
+	 */
+	public String getName()
+	{
+		return name;
+	}
+	
+	/**
+	 * Adds a given value to this Individual
+	 * @param propIndex: the index of the DataProperty associated with this Individual
+	 * @param value: the value of the DataProperty for this Individual
+	 */
+	public void addDataValue(int propIndex, String value)
+	{
+		dataValues.add(propIndex,value);
+	}
+	
+	/**
+	 * @param propIndex: the index of the DataProperty associated with this Individual
+	 * @return the value of the DataProperty for this Individual 
+	 */
+	public Set<String> getDataValue(int propIndex)
+	{
+		return dataValues.get(propIndex);
+	}
+	
+	@Override
+	public String toString()
+	{
+		String s = "name: " + name;
+		return s;
+	}
+}
