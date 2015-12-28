535ff9358c2f96a6d276897c5b29e47c8399c9a5
DanFaria <daniel.faria.81@gmail.com>
2015-12-28 14:23:44 +0000

    Updated isFormula() method

--- AgreementMakerLight/src/aml/util/StringParser.java
+++ AgreementMakerLight/src/aml/util/StringParser.java
@@ -64,7 +64,7 @@ public class StringParser
 	public static boolean isNumericId(String name)
 	{
 		return name.matches("[0-9]*") || name.matches("[0-9]+[_:\\.\\-][0-9]+")
-				|| name.matches("[a-zA-Z]+[_:]?[a-zA-Z]?[0-9]+")
+				|| name.matches("[a-zA-Z]+[_:]?[a-zA-Z]?[0-9\\.]+")
 				|| name.matches("[a-z]{1,3}-[0-9]{3,}-[0-9]{3,}");
 	}
 
