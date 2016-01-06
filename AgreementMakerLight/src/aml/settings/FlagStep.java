d787750095de3ceec9022079c686bb633d2dceb2
DanFaria <daniel.faria.81@gmail.com>
2016-01-06 11:35:02 +0000

    Added FlagStep

---
+++ AgreementMakerLight/src/aml/settings/FlagStep.java
@@ -0,0 +1,47 @@
+/******************************************************************************
+* Copyright 2013-2016 LASIGE                                                  *
+*                                                                             *
+* Licensed under the Apache License, Version 2.0 (the "License"); you may     *
+* not use this file except in compliance with the License. You may obtain a   *
+* copy of the License at http://www.apache.org/licenses/LICENSE-2.0           *
+*                                                                             *
+* Unless required by applicable law or agreed to in writing, software         *
+* distributed under the License is distributed on an "AS IS" BASIS,           *
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    *
+* See the License for the specific language governing permissions and         *
+* limitations under the License.                                              *
+*                                                                             *
+*******************************************************************************
+* Lists the Flagging Steps.                                                   *
+*                                                                             *
+* @author Daniel Faria                                                        *
+******************************************************************************/
+package aml.settings;
+
+public enum FlagStep
+{
+   	CARDINALITY ("Cardinality"),
+   	COHERENCE	("Coherence"),
+   	OBSOLETION	("Obsoletion"),
+   	QUALITY		("Quality");
+	    	
+   	final String value;
+    	
+   	FlagStep(String s)
+   	{
+   		value = s;
+   	}
+	    	
+   	public String toString()
+   	{
+   		return value;
+   	}
+   	
+	public static FlagStep parseStep(String step)
+	{
+		for(FlagStep s : FlagStep.values())
+			if(step.equalsIgnoreCase(s.toString()))
+				return s;
+		return null;
+	}
+}
