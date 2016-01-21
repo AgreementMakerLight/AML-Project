2a628b186d1544fa852edeff3683825ea422df57
DanFaria <daniel.faria.81@gmail.com>
2016-01-21 15:41:35 +0000

    Added rule to isNumericId in StringParser

--- AgreementMakerLight/src/aml/util/StringParser.java
+++ AgreementMakerLight/src/aml/util/StringParser.java
@@ -65,7 +65,8 @@ public class StringParser
 	{
 		return name.matches("[0-9]*") || name.matches("[0-9]+[_:\\.\\-][0-9]+")
 				|| name.matches("[a-zA-Z]+[_:]?[a-zA-Z]?[0-9\\.]+")
-				|| name.matches("[a-z]{1,3}-[0-9]{3,}-[0-9]{3,}");
+				|| name.matches("[a-z]{1,3}-[0-9]{3,}-[0-9]{3,}")
+				|| name.matches("[a-zA-Z]{1}");
 	}
 
 	/**
