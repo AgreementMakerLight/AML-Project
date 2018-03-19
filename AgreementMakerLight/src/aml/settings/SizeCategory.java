6160f95d17aa97efb0e337511094cbdbb0a2a18c
DanielPJFaria <dfaria@xldb.di.fc.ul.pt>
2018-03-19 17:08:38 +0000

    Updated SizeCategory threshold

--- AgreementMakerLight/src/aml/settings/SizeCategory.java
+++ AgreementMakerLight/src/aml/settings/SizeCategory.java
@@ -41,7 +41,8 @@ public enum SizeCategory
 		int sSize = source.count();
 		int tSize = target.count();
 		int max = Math.max(sSize, tSize);
-		if(max > 60000 || sSize*tSize > 1500000000)
+		int min = Math.min(sSize, tSize);
+		if(max > 60000 || (min > 30000 && sSize*tSize > 1000000000))
 			return HUGE;
 		else if(max > 5000)
 			return LARGE;
