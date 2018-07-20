aafef1e08cd55056b0f800ef5c1e37300154bb5b
DanFaria <dfaria@igc.gulbenkian.pt>
2018-07-20 10:00:42 +0100

    Moved MappingStatus

--- AgreementMakerLight/src/aml/alignment/MappingStatus.java
+++ AgreementMakerLight/src/aml/alignment/MappingStatus.java
@@ -16,7 +16,7 @@
 *                                                                             *
 * @author Daniel Faria                                                        *
 ******************************************************************************/
-package aml.settings;
+package aml.alignment;
 
 public enum MappingStatus
 {
