aa397be78d551eec8aa539ce8956889a50c1c2d7
DanFaria <dfaria@igc.gulbenkian.pt>
2018-07-23 17:09:47 +0100

    Updated Similarity

--- AgreementMakerLight/src/aml/util/similarity/Similarity.java
+++ AgreementMakerLight/src/aml/util/similarity/Similarity.java
@@ -19,6 +19,12 @@
 package aml.util.similarity;
 
 import java.util.Collection;
+import java.util.HashSet;
+
+import aml.settings.StringSimMeasure;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.JaroWinkler;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.Levenshtein;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.QGramsDistance;
 
 public class Similarity
 {
@@ -30,7 +36,7 @@ public class Similarity
 	 * @param c2: the second Collection
 	 * @return the Jaccard similarity between c1 and c2
 	 */
-	public static <X extends Object> double jaccard(Collection<X> c1, Collection<X> c2)
+	public static <X extends Object> double jaccardSimilarity(Collection<X> c1, Collection<X> c2)
 	{
 		if(c1.size() == 0 || c2.size() == 0)
 			return 0.0;
@@ -45,5 +51,99 @@ public class Similarity
 		}
 		union += c2.size();
 		return intersection/union;
+	}
+	
+	/**
+	 * Computes the similarity between two names using a combination of String-, Word-
+	 * and WordNet-Similarity measures
+	 * @param n1: the first name to compare
+	 * @param n2: the second name to compare
+	 * @param useWord: whether to use the WordNet
+	 * @return the similarity between the two names
+	 */
+	public static double nameSimilarity(String n1, String n2, boolean useWordNet)
+	{
+		WordNet wn = null;
+		if(useWordNet)
+			wn = new WordNet();
+		//We don't compare two-character names at all
+		if(n1.length() < 3 || n2.length() < 3)
+			return 0.0;
+		//If the names are equal, no need to compute similarity
+		if(n1.equals(n2))
+			return 1.0;
+		
+		//Split the source name into words
+		String[] sW = n1.split(" ");
+		HashSet<String> sWords = new HashSet<String>();
+		HashSet<String> sSyns = new HashSet<String>();
+		for(String w : sW)
+		{
+			sWords.add(w);
+			sSyns.add(w);
+			//And compute the WordNet synonyms of each word
+			if(wn != null && w.length() > 2)
+				sSyns.addAll(wn.getAllNounWordForms(w));
+		}
+		
+		//Split the target name into words
+		String[] tW = n2.split(" ");
+		HashSet<String> tWords = new HashSet<String>();
+		HashSet<String> tSyns = new HashSet<String>();		
+		for(String w : tW)
+		{
+			tWords.add(w);
+			tSyns.add(w);
+			//And compute the WordNet synonyms of each word
+			if(wn != null && w.length() > 3)
+				tSyns.addAll(wn.getAllWordForms(w));
+		}
+		
+		//Compute the Jaccard word similarity between the properties
+		double wordSim = Similarity.jaccardSimilarity(sWords,tWords)*0.9;
+		//and the String similarity
+		double simString = ISub.stringSimilarity(n1,n2)*0.9;
+		//Combine the two
+		double sim = 1 - ((1-wordSim) * (1-simString));
+		if(wn != null)
+		{
+			//Check if the WordNet similarity
+			double wordNetSim = Similarity.jaccardSimilarity(sSyns,tSyns);
+			//Is greater than the name similarity
+			if(wordNetSim > sim)
+				//And if so, return it
+				sim = wordNetSim;
+		}
+		return sim;
+	}
+	
+	/**
+	 * Computes the string the similarity between two Strings
+	 * @param s: the first String
+	 * @param t: the second String
+	 * @param measure: the String Similarity Measure
+	 * @return
+	 */
+	public static double stringSimilarity(String s, String t, String measure)
+	{
+		double sim = 0.0;
+		if(measure.equals(StringSimMeasure.ISUB))
+			sim = ISub.stringSimilarity(s,t);
+		else if(measure.equals(StringSimMeasure.EDIT))
+		{
+			Levenshtein lv = new Levenshtein();
+			sim = lv.getSimilarity(s, t);
+		}
+		else if(measure.equals(StringSimMeasure.JW))
+		{
+			JaroWinkler jv = new JaroWinkler();
+			sim = jv.getSimilarity(s, t);
+		}
+		else if(measure.equals(StringSimMeasure.QGRAM))
+		{
+			QGramsDistance q = new QGramsDistance();
+			sim = q.getSimilarity(s, t);
+		}
+		return sim;
 	}
 }
