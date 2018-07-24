20df56e41b8a0ba5180bdf3b3c8251cb553a6700
DanFaria <daniel.faria.81@gmail.com>
2018-07-24 13:40:10 +0100

    Created AbstractAlignment

---
+++ AgreementMakerLight/src/aml/alignment/AbstractAlignment.java
@@ -0,0 +1,610 @@
+/******************************************************************************
+* Copyright 2013-2018 LASIGE                                                  *
+*                                                                             *
+* Licensed under the Apache License, Version 2.0 (the "License"); you may     *
+* not use this file except in compliance with the License. You may obtain a   *
+* copy of the License at http://www.apache.org/licenses/LICENSE-2.0           *
+*                                                                             *
+* Unless required by applicable law or agreed to in writing, software         *
+* distributed under the License is distributed on an "AS IS" BASIS,           *
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    *
+* See the License for the specific language governing permissions and         *
+* limitations under the License.                                              *
+*                                                                             *
+*******************************************************************************
+* An alignment between two Ontologies, represented as a list of Mappings.     *
+*                                                                             *
+* @author Daniel Faria                                                        *
+******************************************************************************/
+package aml.alignment;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Vector;
+
+import aml.AML;
+import aml.ontology.EntityType;
+
+public abstract class AbstractAlignment implements Collection<AbstractMapping>
+{
+
+//Attributes
+
+	//Ontology uris (as listed in alignment file)
+	protected String sourceURI;
+	protected String targetURI;
+	//Mappings organized in list
+	protected Vector<AbstractMapping> maps;
+	
+//Constructors
+
+	/**
+	 * Creates a new empty Alignment between the source and target ontologies
+	 */
+	public AbstractAlignment()
+	{
+		this(AML.getInstance().getSource().getURI(), AML.getInstance().getTarget().getURI());
+	}
+
+	/**
+	 * Creates a new empty Alignment
+	 */
+	public AbstractAlignment(String sourceUri, String targetUri)
+	{
+		this.sourceURI = sourceUri;
+		this.targetURI = targetUri;
+		maps = new Vector<AbstractMapping>(0,1);
+	}
+
+	/**
+	 * Creates a new Alignment that contains the input collection of mappings
+	 * @param a: the collection of mappings to include in this Alignment
+	 */
+	public AbstractAlignment(Collection<AbstractMapping> a)
+	{
+		this();
+		addAll(a);
+	}
+	
+//Public Methods
+
+	/**
+	 * Adds a new AbstractMapping to the AbstractAlignment
+	 * @param entity1: the entity1 to add to the Alignment
+	 * @param entity2: the entity2 to add to the Alignment
+	 * @param sim: the similarity between the classes
+	 */
+	public abstract boolean add(Object entity1, Object entity2, double sim);
+	
+	/**
+	 * Adds a new AbstractMapping to the AbstractAlignment
+	 * @param entity1: the entity1 to add to the Alignment
+	 * @param entity2: the entity2 to add to the Alignment
+	 * @param sim: the similarity between the classes
+	 * @param r: the mapping relationship between the classes
+	 */
+	public abstract boolean add(Object entity1, Object entity2, double sim, MappingRelation r);
+	
+	/**
+	 * Adds a new AbstractMapping to the AbstractAlignment
+	 * @param entity1: the entity1 to add to the Alignment
+	 * @param entity2: the entity2 to add to the Alignment
+	 * @param sim: the similarity between the classes
+	 * @param r: the mapping relationship between the classes
+	 * @param s: the mapping status
+	 */
+	public abstract boolean add(Object entity1, Object entity2, double sim, MappingRelation r, MappingStatus s);
+	
+	@Override
+	public boolean add(AbstractMapping m)
+	{
+		return maps.add(m);
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends AbstractMapping> a)
+	{
+		return maps.addAll(a);
+	}
+	
+	/**
+	 * Adds all Mappings in a to this Alignment as long as
+	 * they don't conflict with any Mapping already present
+	 * in this Alignment
+	 * @param a: the collection of Mappings to add to this Alignment
+	 */
+	public void addAllNonConflicting(Collection<? extends AbstractMapping> a)
+	{
+		Vector<AbstractMapping> nonConflicting = new Vector<AbstractMapping>();
+		for(AbstractMapping m : a)
+			if(!this.containsConflict(m))
+				nonConflicting.add(m);
+		addAll(nonConflicting);
+	}
+	
+	/**
+	 * Adds all Mappings in a to this Alignment in descending
+	 * order of similarity, as long as they don't conflict with
+	 * any Mapping already present or previously added to this
+	 * Alignment
+	 * @param a: the Alignment to add to this Alignment
+	 */
+	public void addAllOneToOne(AbstractAlignment a)
+	{
+		a.sortDescending();
+		for(AbstractMapping m : a.maps)
+			if(!this.containsConflict(m))
+				add(m);
+	}
+	
+	/**
+	 * @return the average cardinality of this Alignment
+	 */
+	public abstract double cardinality();
+	
+	/**
+	 * @param uri: the entity to check in the Alignment
+	 * @return the cardinality of the entity in the Alignment
+	 */
+	public abstract int cardinality(String uri);
+	
+	@Override
+	public void clear()
+	{
+		maps = new Vector<AbstractMapping>(0,1);
+	}
+	
+	@Override
+	public boolean contains(Object o)
+	{
+		return o instanceof AbstractMapping && maps.contains((SimpleMapping)o);
+	}
+	
+	/**
+	 * @param entity1: the entity1 to check in the Alignment
+ 	 * @param entity2: the entity2 to check in the Alignment
+	 * @return whether the Alignment contains a Mapping between entity1 and entity2
+	 */
+	public boolean contains(Object entity1, Object entity2)
+	{
+		for(AbstractMapping m : maps)
+			if(m.getEntity1().equals(entity1) && m.getEntity2().equals(entity2))
+				return true;
+		return false;
+	}
+	
+	/**
+	 * @param entity1: the entity1 to check in the Alignment
+ 	 * @param entity2: the entity2 to check in the Alignment
+ 	 * @param r: the MappingRelation to check in the Alignment
+	 * @return whether the Alignment contains a Mapping between entity1 and entity2
+	 * with relationship r
+	 */
+	public boolean contains(Object entity1, Object entity2, MappingRelation r)
+	{
+		for(AbstractMapping m : maps)
+			if(m.getEntity1().equals(entity1) && m.getEntity2().equals(entity2) && m.getRelationship().equals(r))
+				return true;
+		return false;
+	}
+	
+	@Override
+	public boolean containsAll(Collection<?> c)
+	{
+		for(Object o : c)
+			if(!contains(o))
+				return false;
+		return true;
+	}
+	
+	/**
+ 	 * @param m: the Mapping to check in the Alignment 
+	 * @return whether the Alignment contains a Mapping that conflicts with the given
+	 * Mapping and has a higher similarity
+	 */
+	public abstract boolean containsBetterMapping(AbstractMapping m);
+	
+	/**
+ 	 * @param entity: the entity to check in the Alignment 
+	 * @return whether the Alignment contains a Mapping with that entity
+	 * (either as entity1 or entity2)
+	 */
+	public boolean containsEntity(Object entity)
+	{
+		return containsSource(entity) || containsTarget(entity);
+	}
+	
+	/**
+	 * @param entity1: the entity1 to check in the Alignment
+ 	 * @param entity2: the entity2 to check in the Alignment 
+	 * @return whether the Alignment contains another Mapping for entity1 or for entity2
+	 */
+	public abstract boolean containsConflict(Object entity1, Object entity2);
+	
+	/**
+ 	 * @param m: the Mapping to check in the Alignment 
+	 * @return whether the Alignment contains another Mapping involving either entity in m
+	 */
+	public boolean containsConflict(AbstractMapping m)
+	{
+		return containsConflict(m.getEntity1(),m.getEntity2());
+	}
+	
+	/**
+	 * @param entity1: the entity1 to check in the Alignment
+ 	 * @return whether the Alignment contains a Mapping for entity1
+	 */
+	public boolean containsSource(Object entity1)
+	{
+		for(AbstractMapping m : maps)
+			if(m.getEntity1().equals(entity1))
+				return true;
+		return false;
+	}
+
+	/**
+	 * @param entity2: the entity2 to check in the Alignment
+ 	 * @return whether the Alignment contains a Mapping for entity1
+	 */
+	public boolean containsTarget(Object entity2)
+	{
+		for(AbstractMapping m : maps)
+			if(m.getEntity2().equals(entity2))
+				return true;
+		return false;
+	}
+	
+	/**
+ 	 * @return the number of conflict mappings in this alignment
+	 */
+	public int countConflicts()
+	{
+		int count = 0;
+		for(AbstractMapping m : maps)
+			if(m.getRelationship().equals(MappingRelation.UNKNOWN))
+				count++;
+		return count;
+	}
+	
+	/**
+	 * @param a: the Alignment to subtract from this Alignment 
+	 * @return the Alignment corresponding to the difference between this Alignment and a
+	 */
+	public abstract AbstractAlignment difference(AbstractAlignment a);
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		return o instanceof AbstractAlignment && containsAll((AbstractAlignment)o);
+	}
+	
+	/**
+	 * @param ref: the reference Alignment to evaluate this Alignment
+	 * @return the evaluation of this Alignment {# correct mappings, # conflict mappings}
+	 */
+	public int[] evaluate(AbstractAlignment ref)
+	{
+		int[] count = new int[2];
+		for(AbstractMapping m : maps)
+		{
+			if(ref.contains(m))
+			{
+				count[0]++;
+				m.setStatus(MappingStatus.CORRECT);
+			}
+			else if(ref.contains((String)m.getEntity1(),(String)m.getEntity2(),MappingRelation.UNKNOWN))
+			{
+				count[1]++;
+				m.setStatus(MappingStatus.UNKNOWN);
+			}
+			else
+				m.setStatus(MappingStatus.INCORRECT);
+		}
+		return count;
+	}
+
+	/**
+	 * @param a: the base Alignment to which this Alignment will be compared 
+	 * @return the gain (i.e. the fraction of new Mappings) of this Alignment
+	 * in comparison with the base Alignment
+	 */
+	public double gain(AbstractAlignment a)
+	{
+		double gain = 0.0;
+		for(AbstractMapping m : maps)
+			if(!a.contains(m))
+				gain++;
+		gain /= a.size();
+		return gain;
+	}
+	
+	/**
+	 * @param a: the base Alignment to which this Alignment will be compared 
+	 * @return the gain (i.e. the fraction of new Mappings) of this Alignment
+	 * in comparison with the base Alignment
+	 */
+	public abstract double gainOneToOne(AbstractAlignment a);
+	
+	/**
+	 * @param index: the index of the Mapping to return in the list of Mappings
+ 	 * @return the Mapping at the input index (note that the index will change
+ 	 * during sorting) or null if the uri falls outside the list
+	 */
+	public AbstractMapping get(int index)
+	{
+		if(index < 0 || index >= maps.size())
+			return null;
+		return maps.get(index);
+	}
+	
+	/**
+	 * @param entity1: the entity1 to check in the Alignment
+	 * @param entity2: the entity2 to check in the Alignment
+ 	 * @return the Mapping between the entity1 and entity2 classes or null if no
+ 	 * such Mapping exists
+	 */
+	public abstract AbstractMapping get(Object entity1, Object entity2);
+	
+	/**
+	 * @param m: the Mapping to check on the Alignment
+	 * @return the list of all Mappings that have a cardinality conflict with the given Mapping
+	 */
+	public abstract Vector<AbstractMapping> getConflicts(AbstractMapping m);
+	
+	/**
+	 * @param entity1: the entity1
+	 * @param entity2: the entity2
+	 * @return the uri of the Mapping between the given classes in
+	 * the list of Mappings, or -1 if the Mapping doesn't exist
+	 */
+	public int getIndex(Object entity1, Object entity2)
+	{
+		for(int i = 0; i < maps.size(); i++)
+		{
+			AbstractMapping m = maps.get(i);
+			if(m.getEntity1().equals(entity1) && m.getEntity2().equals(entity2))
+				return i;
+		}
+		return -1;
+	}
+	
+	/**
+	 * @param entity: the entity to check in the Alignment
+ 	 * @return the set of entities mapped to the given entity
+	 */
+	public Set<Object> getMappingsBidirectional(Object entity)
+	{
+		HashSet<Object> mappings = new HashSet<Object>();
+		for(AbstractMapping m : maps)
+		{
+			if(m.getEntity1().equals(entity))
+				mappings.add(m.getEntity2());
+			else if(m.getEntity2().equals(entity))
+				mappings.add(m.getEntity1());
+		}
+		return mappings;
+	}
+	
+	/**
+	 * @param entity1: the entity1 to check in the Alignment
+ 	 * @return the highest similarity in mappings involving entity1
+	 */
+	public abstract double getMaxSourceSim(Object entity1);
+
+	/**
+	 * @param entity2: the entity2 to check in the Alignment
+ 	 * @return the highest similarity in mappings involving entity2
+	 */
+	public abstract double getMaxTargetSim(String entity2);
+	
+	/**
+	 * @param entity1: the entity1 in the Alignment
+	 * @param entity2: the entity2 in the Alignment
+	 * @return the mapping relationship between entity1 and entity2
+	 */
+	public MappingRelation getRelationship(String entity1, String entity2)
+	{
+		AbstractMapping m = get(entity1, entity2);
+		if(m == null)
+			return null;
+		return m.getRelationship();
+	}
+	
+	/**
+	 * @param entity1: the entity1 in the Alignment
+	 * @param entity2: the entity2 in the Alignment
+	 * @return the similarity between entity1 and entity2
+	 */
+	public double getSimilarity(String entity1, String entity2)
+	{
+		AbstractMapping m = get(entity1, entity2);
+		if(m == null)
+			return 0.0;
+		return m.getSimilarity();
+	}
+	
+	/**
+	 * @param entity1: the entity1 in the Alignment
+	 * @param entity2: the entity2 in the Alignment
+	 * @return the similarity between entity1 and entity2 in percentage
+	 */
+	public String getSimilarityPercent(String entity1, String entity2)
+	{
+		AbstractMapping m = get(entity1, entity2);
+		if(m == null)
+			return "0%";
+		return m.getSimilarityPercent();
+	}
+	
+	/**
+	 * @param entity1: the entity1 to check in the Alignment
+ 	 * @return the list of all entity2 mapped to entity1
+	 */
+	public abstract Set<Object> getSourceMappings(Object entity1);
+	
+	/**
+ 	 * @return the list of all entity1 that have mappings
+	 */
+	public abstract Set<Object> getSources();
+
+	/**
+	 * @return the URI of the source ontology
+	 */
+	public String getSourceURI()
+	{
+		return sourceURI;
+	}
+	
+	/**
+	 * @param entity2: the entity2 to check in the Alignment
+ 	 * @return the list of all entity1 mapped to entity2
+	 */
+	public abstract Set<Object> getTargetMappings(Object entity2);
+	
+	/**
+ 	 * @return the list of all entity2 that have mappings
+	 */
+	public abstract Set<Object> getTargets();
+	
+	/**
+	 * @return the URI of the target ontology
+	 */
+	public String getTargetURI()
+	{
+		return targetURI;
+	}
+
+
+	@Override
+	public int hashCode()
+	{
+		return maps.hashCode();
+	}
+	
+	/**
+	 * @param a: the Alignment to intersect with this Alignment 
+	 * @return the Alignment corresponding to the intersection between this Alignment and a
+	 */
+	public abstract AbstractAlignment intersection(AbstractAlignment a);
+	
+	@Override
+	public boolean isEmpty()
+	{
+		return maps.isEmpty();
+	}
+	
+	@Override
+	public Iterator<AbstractMapping> iterator()
+	{
+		return maps.iterator();
+	}
+	
+	/**
+	 * @return the maximum cardinality of this Alignment
+	 */
+	public abstract double maxCardinality();
+	
+	@Override
+	public boolean remove(Object o)
+	{
+		return maps.remove(o);
+	}
+	
+	/**
+	 * Removes the Mapping between the given classes from the Alignment
+	 * @param entity1: the entity1 to remove from the Alignment
+	 * @param entity2: the entity2 to remove from the Alignment
+	 */
+	public boolean remove(String entity1, String entity2)
+	{
+		AbstractMapping m = new SimpleMapping(entity1, entity2, 1.0);
+		return remove(m);
+	}
+	
+	@Override
+	public boolean removeAll(Collection<?> c)
+	{
+		boolean check = false;
+		for(Object o : c)
+			check = remove(o) || check;
+		return check;
+	}
+	
+	@Override
+	public boolean retainAll(Collection<?> c)
+	{
+		boolean check = false;
+		for(AbstractMapping m : this)
+			if(!c.contains(m))
+				check = remove(m) || check;
+		return check;
+	}
+	
+	@Override
+	public int size()
+	{
+		return maps.size();
+	}
+
+	/**
+	 * Sorts the Alignment ascendingly
+	 */
+	public void sortAscending()
+	{
+		Collections.sort(maps);
+	}
+	
+	/**
+	 * Sorts the Alignment descendingly
+	 */
+	public void sortDescending()
+	{
+		Collections.sort(maps,new Comparator<AbstractMapping>()
+        {
+			//Sorting in descending order can be done simply by
+			//reversing the order of the elements in the comparison
+            public int compare(AbstractMapping m1, AbstractMapping m2)
+            {
+        		return m2.compareTo(m1);
+            }
+        } );
+	}
+	
+	/**
+	 * @return the number of entity1 mapped in this Alignment
+	 */
+	public abstract int sourceCount();
+	
+	/**
+	 * @param e: the EntityType to search in the Alignment
+	 * @return the fraction of entity1 of the given type mapped in this Alignment
+	 */
+	public abstract double sourceCoverage(EntityType e);
+	
+	/**
+	 * @return the number of entity2 mapped in this Alignment
+	 */
+	public abstract int targetCount();
+	
+	/**
+	 * @param e: the EntityType to search in the Alignment
+	 * @return the fraction of entity2 of the given type mapped in this Alignment
+	 */
+	public abstract double targetCoverage(EntityType e);
+	
+	@Override
+	public Object[] toArray()
+	{
+		return maps.toArray();
+	}
+	
+	@Override
+	public <T> T[] toArray(T[] a)
+	{
+		return maps.toArray(a);
+	}
+}
