061a151f71bbcac132fb00a3a5b79d8a3086e131
DanFaria <dfaria@igc.gulbenkian.pt>
2018-07-25 19:58:33 +0100

    Added composition expressions

---
+++ AgreementMakerLight/src/aml/alignment/edoal/PropertyComposition.java
@@ -0,0 +1,81 @@
+/******************************************************************************
+* Copyright 2013-2018 LASIGE                                                  *
+*                                                                             *
+* Licensed under the Apache License, Version 2.0 (the "License"); you may     *
+* not use this file except in compliance with the License. You may obtain a   *
+* copy of the License at http://www.apache.org/licenses/LICENSE-2.0           *
+*                                                                             *
+* Unless required by applicable law or agreed to in writing, software         *
+* distributed under the License is distributed on an "AS IS" BASIS,           *
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    *
+* See the License for the specific language governing permissions and         *
+* limitations under the License.                                              *
+*                                                                             *
+*******************************************************************************
+* An EDOAL Property Composition is an ordered path of Relations ending with   *
+* a Property. It is equivalent to an OWL Object Property Chain, except that   *
+* the last element is a Data Property.                                        *
+*                                                                             *
+* @author Daniel Faria                                                        *
+******************************************************************************/
+package aml.alignment.edoal;
+
+import java.util.Vector;
+
+public class PropertyComposition extends PropertyExpression
+{
+
+//Attributes
+	
+	private Vector<RelationExpression> path;
+	private PropertyExpression end;
+	
+//Constructor
+	
+	/**
+	 * Constructs a new PropertyComposition from the given list of relation expressions plus property expression
+	 * @param path: the list of relation expressions in the composition
+	 * @param end: the property expression that is the last element of the composition
+	 */
+	public PropertyComposition(Vector<RelationExpression> path, PropertyExpression end)
+	{
+		this.path = path;
+		this.end = end;
+		for(RelationExpression e : path)
+			elements.addAll(e.getElements());
+		elements.addAll(end.getElements());
+	}
+	
+//Public Methods
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		return o instanceof PropertyComposition &&
+				((PropertyComposition)o).end.equals(this.end) &&
+				((PropertyComposition)o).path.equals(this.path);
+	}
+	
+	@Override
+	public String toRDF()
+	{
+		String rdf = "<edoal:Property>\n" +
+				"<edoal:compose rdf:parseType=\"Collection\">\n";
+		for(RelationExpression e : path)
+			rdf += e.toRDF() + "\n";
+		rdf += end.toRDF() + "\n";
+		rdf += "</edoal:compose>\n";
+		rdf += "</edoal:Property>\n";
+		return rdf;
+	}
+
+	@Override
+	public String toString()
+	{
+		String s = "PATH[";
+		for(RelationExpression e : path)
+			s += e.toString() + ", ";
+		s += end.toString() + "]";
+		return s;
+	}
+}
---
+++ AgreementMakerLight/src/aml/alignment/edoal/RelationComposition.java
@@ -0,0 +1,74 @@
+/******************************************************************************
+* Copyright 2013-2018 LASIGE                                                  *
+*                                                                             *
+* Licensed under the Apache License, Version 2.0 (the "License"); you may     *
+* not use this file except in compliance with the License. You may obtain a   *
+* copy of the License at http://www.apache.org/licenses/LICENSE-2.0           *
+*                                                                             *
+* Unless required by applicable law or agreed to in writing, software         *
+* distributed under the License is distributed on an "AS IS" BASIS,           *
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    *
+* See the License for the specific language governing permissions and         *
+* limitations under the License.                                              *
+*                                                                             *
+*******************************************************************************
+* An EDOAL Relation Composition is an ordered path of Relations that is       *
+* equivalent to an OWL Object Relation Chain.                                 *
+*                                                                             *
+* @author Daniel Faria                                                        *
+******************************************************************************/
+package aml.alignment.edoal;
+
+import java.util.Vector;
+
+public class RelationComposition extends RelationExpression
+{
+
+//Attributes
+	
+	private Vector<RelationExpression> path;
+	
+//Constructor
+	
+	/**
+	 * Constructs a new RelationComposition from the given list of relation expressions
+	 * @param path: the list of relation expressions in this composition
+	 */
+	public RelationComposition(Vector<RelationExpression> path)
+	{
+		this.path = path;
+		for(RelationExpression e : path)
+			elements.addAll(e.getElements());
+	}
+	
+//Public Methods
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		return o instanceof RelationComposition &&
+				((RelationComposition)o).path.equals(this.path);
+	}
+	
+	@Override
+	public String toRDF()
+	{
+		String rdf = "<edoal:Relation>\n" +
+				"<edoal:compose rdf:parseType=\"Collection\">\n";
+		for(RelationExpression e : path)
+			rdf += e.toRDF() + "\n";
+		rdf += "</edoal:compose>\n";
+		rdf += "</edoal:Relation>\n";
+		return rdf;
+	}
+
+	@Override
+	public String toString()
+	{
+		String s = "PATH[";
+		for(RelationExpression e : path)
+			s += e.toString() + ", ";
+		s = s.substring(0, s.lastIndexOf(',')) + "]";
+		return s;
+	}
+}
