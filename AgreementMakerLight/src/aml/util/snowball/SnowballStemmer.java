3b13917f20d563a7aefcee00a4382aed4672b727
DanFaria <dfaria@igc.gulbenkian.pt>
2018-09-09 17:36:21 +0100

    Added snowball stemmer

---
+++ AgreementMakerLight/src/aml/util/snowball/SnowballStemmer.java
@@ -0,0 +1,7 @@
+
+package aml.util.snowball;
+import java.lang.reflect.InvocationTargetException;
+
+public abstract class SnowballStemmer extends SnowballProgram {
+    public abstract boolean stem();
+};
