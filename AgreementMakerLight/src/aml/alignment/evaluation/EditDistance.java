32cf93455f1fc2133143b856b88a1b658fa39dd1
beatriz-lima <beatriz.f.l@hotmail.com>
2021-08-03 12:45:17 +0100

    Fixed bug and cover more constructs

--- AgreementMakerLight/src/aml/alignment/evaluation/EditDistance.java
+++ AgreementMakerLight/src/aml/alignment/evaluation/EditDistance.java
@@ -1,9 +1,13 @@
 package aml.alignment.evaluation;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import aml.alignment.Alignment;
 import aml.alignment.EDOALAlignment;
@@ -24,98 +28,77 @@ public class EditDistance implements Evaluator
 	 * @param alignment: the Alignment we want to evaluate
 	 * @param reference: the reference Alignment that we will use for comparison
 	 * @return the evaluation of this Alignment {#TP for precision, #conflict mappings, #TP for recall}
+	 * @throws IOException 
 	 */
 	@SuppressWarnings("rawtypes")
-	public double[] evaluate(Alignment alignment, Alignment reference) 
+	public double[] evaluate(Alignment alignment, Alignment reference) throws IOException 
 	{
 		System.out.println("Performing Evaluation");
 		long time = System.currentTimeMillis()/1000;
 		EDOALAlignment algn = (EDOALAlignment)alignment;
 		EDOALAlignment ref = (EDOALAlignment)reference;
 		HashMap<Mapping<AbstractExpression>, Double> usedRefMappings = new HashMap<Mapping<AbstractExpression>, Double>();
-
+		
 		double[] count = new double[3];
 		for(Mapping<AbstractExpression> m : algn) 
-		{
-			// Exact matches
-			if(ref.contains(m))
+		{	
+			// Construct a vector containing the single entities, relationship and constructors of the mapping being evaluated
+			Vector<String> algnElements = new Vector<String>(m.getEntity1().getElements());
+			algnElements.addAll(m.getEntity2().getElements());
+			algnElements.add(m.getRelationship().toString());
+			addConstructors(m, algnElements);
+
+			// Choose which reference mappings to compare it with
+			Vector<Mapping<AbstractExpression>> srcRefMappings = new Vector<Mapping<AbstractExpression>>();
+			Vector<Mapping<AbstractExpression>> tgtRefMappings = new Vector<Mapping<AbstractExpression>>();
+
+			for (String e: m.getEntity1().getElements()) 
+				srcRefMappings.addAll(ref.getSourceMappings(e));	 
+			for (String e: m.getEntity2().getElements()) 
+				tgtRefMappings = ref.getTargetMappings(e);
+			if(srcRefMappings.size()==0 || tgtRefMappings.size()==0)
+				continue;
+
+			// Only consider reference mappings that have at least one common 
+			// src and tgt entity to the mapping being evaluated
+			srcRefMappings.retainAll(tgtRefMappings);
+			if(srcRefMappings.size()==0)
+				continue;
+
+			// Find the maximum Jaccard similarity between the ref mappings and the mapping being evaluated
+			double max = 0;
+			Mapping<AbstractExpression> chosenRef = null;
+
+			for (Mapping<AbstractExpression> mRef: srcRefMappings) 
 			{
-				System.out.println("Exact match!");
-				count[0]++;
-				m.setStatus(MappingStatus.CORRECT);
-				if(!usedRefMappings.containsKey(m))
-					usedRefMappings.put(m, 1.0);
-				else if(usedRefMappings.get(m)< 1.0)
-					usedRefMappings.put(m, 1.0);
+				// Construct a vector containing the single entities, relationship and constructors of the reference mapping
+				Vector<String> refElements = new Vector<String>(mRef.getEntity1().getElements());
+				refElements.addAll(mRef.getEntity2().getElements());
+				refElements.add(mRef.getRelationship().toString());
+				addConstructors(mRef, refElements);
 
-			}
-			// Jaccard similarity
-			else
-			{
-				// Construct a vector containing the single entities, relationship and constructors of the mapping being evaluated
-				Vector<String> algnElements = new Vector<String>(m.getEntity1().getElements());
-				algnElements.addAll(m.getEntity2().getElements());
-				algnElements.add(m.getRelationship().toString());
-				addConstructors(m, algnElements);
-
-				// Choose which reference mappings to compare it with
-				Vector<Mapping<AbstractExpression>> srcRefMappings = new Vector<Mapping<AbstractExpression>>();
-				Vector<Mapping<AbstractExpression>> tgtRefMappings = new Vector<Mapping<AbstractExpression>>();
-
-				for (String e: m.getEntity1().getElements()) 
-					srcRefMappings.addAll(ref.getSourceMappings(e));	 
-				for (String e: m.getEntity2().getElements()) 
-					tgtRefMappings = ref.getTargetMappings(e);
-				if(srcRefMappings.size()==0 || tgtRefMappings.size()==0)
-					continue;
-
-				// Only consider reference mappings that have at least one common 
-				// src and tgt entity to the mapping being evaluated
-				srcRefMappings.retainAll(tgtRefMappings);
-				if(srcRefMappings.size()==0)
-					continue;
-
-				System.out.println("------------------------");
-				System.out.println("algnMapping: "+m);
-				System.out.println("refMappings: "+srcRefMappings);
-
-				// Find the maximum Jaccard similarity between the ref mappings and the mapping being evaluated
-				double max = 0;
-				Mapping<AbstractExpression> chosenRef = null;
-
-				for (Mapping<AbstractExpression> mRef: srcRefMappings) 
+				double sim = Similarity.weightedJaccardSimilarity(algnElements, refElements);
+				if (sim > max) 
 				{
-					// Construct a vector containing the single entities, relationship and constructors of the reference mapping
-					Vector<String> refElements = new Vector<String>(mRef.getEntity1().getElements());
-					refElements.addAll(mRef.getEntity2().getElements());
-					refElements.add(mRef.getRelationship().toString());
-					addConstructors(mRef, refElements);
-
-					System.out.println("refSet"+refElements);
-					double sim = Similarity.weightedJaccardSimilarity(algnElements, refElements);
-					if (sim > max) 
-					{
-						max = sim;
-						chosenRef = mRef;
-					}
+					max = sim;
+					chosenRef = mRef;
 				}
-				System.out.println("Jaccard: "+ max);
-				count[0]+= max;
-				if(!usedRefMappings.containsKey(chosenRef))
-					usedRefMappings.put(chosenRef, max);
-				else if(usedRefMappings.get(chosenRef)< max)
-					usedRefMappings.put(chosenRef, max);	
 			}
+			count[0]+= max;
+			if(!usedRefMappings.containsKey(chosenRef))
+				usedRefMappings.put(chosenRef, max);
+			else if(usedRefMappings.get(chosenRef)< max)
+				usedRefMappings.put(chosenRef, max);
 		}
-		
+
 		// sum usedRefMappings max scores to obtain recall correct TP
 		for(Mapping<AbstractExpression> m: usedRefMappings.keySet()) 
 			count[2] += usedRefMappings.get(m);
-		
-		System.out.println("# used reference mappings: "+ usedRefMappings.size());
-		System.out.println("recall score: "+ count[2]);
-		System.out.println("precision score: "+ count[0]);
-		System.out.println("Finished in " +	(System.currentTimeMillis()/1000-time) + " seconds");
+
+//		System.out.println("# used reference mappings: "+ usedRefMappings.size());
+//		System.out.println("recall score: "+ count[2]);
+//		System.out.println("precision score: "+ count[0]);
+//		System.out.println("Finished in " +	(System.currentTimeMillis()/1000-time) + " seconds");
 		return count;
 	}
 
@@ -129,13 +112,18 @@ public class EditDistance implements Evaluator
 		Vector<String> constructors = new Vector<String>();
 		String m = mapping.toString();
 
+		// Constructors
 		constructors.addAll(searchKeyword(m, "AND"));
 		constructors.addAll(searchKeyword(m, "OR"));
 		constructors.addAll(searchKeyword(m, "NOT"));
 		constructors.addAll(searchKeyword(m, "PATH"));
 		constructors.addAll(searchKeyword(m, "INVERSE"));
 		constructors.addAll(searchKeyword(m, "COMPOSE"));
+
+		//Occurrence/value restrictions
 		constructors.addAll(searchKeyword(m, "greater-than "));
+		constructors.addAll(searchKeyword(m, "lower-than "));
+		constructors.addAll(searchKeyword(m, "equals "));
 		elements.addAll(constructors);
 	}
 
@@ -152,12 +140,17 @@ public class EditDistance implements Evaluator
 
 		if (split.length > 1) 
 		{
-			if(keyword =="greater-than ") 
+			if(keyword.equals("greater-than ") || keyword.equals("lower-than ") || 
+					keyword.equals("equals ")) 
 			{
-				keyword += split[1].substring(1, 2);
+				result.add(keyword);
+				Pattern regex = Pattern.compile("\".*?\"");
+				Matcher regexMatcher = regex.matcher(s);
+				while (regexMatcher.find()) {//Finds Matching Pattern in String
+					   result.add(regexMatcher.group(0));//Fetching Group from String
+					}
 			}
 			// also add other value expressions
-
 			for (int i=0; i<(split.length-1); i++) 
 				result.add(keyword);
 		}
